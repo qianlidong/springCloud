3cdfe8f572389c49e518d21fcf9c5b1195fc221d
qianlidong <qianlidongjava@163.com>
2017-10-12 09:41:35 +0800

    springCloud第一次提交

---
+++ src/main/java/com/qian/EurekaserverApplication.java
@@ -0,0 +1,17 @@
+package com.qian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动一个服务注册中心EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+}
